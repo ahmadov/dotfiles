b71efce83cc9f24b276af2a6535046955d5b942c
Elmi Ahmadov <elmi.ahmadov@clickhouse.com>
2025-06-24 18:54:14 +0200

    Update ch functions

--- fish/ch-functions.fish
+++ fish/ch-functions.fish
@@ -1,101 +1,116 @@
 #!/bin/fish
 
+switch (uname)
+case Darwin
+  set CH_CC_COMPILER -DCMAKE_C_COMPILER=/opt/homebrew/opt/llvm/bin/clang-19
+  set CH_CXX_COMPILER -DCMAKE_CXX_COMPILER=/opt/homebrew/opt/llvm/bin/clang++-19
+case Linux
+  set CH_CC_COMPILER -DCMAKE_C_COMPILER=/usr/lib/ccache/clang-19
+  set CH_CXX_COMPILER -DCMAKE_CXX_COMPILER=/usr/lib/ccache/clang++-19
+end
+
 set CH_REPO_PATH ~/git/ClickHouse
-set CH_DEBUG_PATH $CH_REPO_PATH/build/Debug
-set CH_RELEASE_PATH $CH_REPO_PATH/build/Release
 
-set CH_COMMON "-DENABLE_BUILD_PATH_MAPPING=0 -DENABLE_TESTS=0"
+set CH_EXTRA_ARGS 
+set CH_COMMON_ARGS -DENABLE_BUILD_PATH_MAPPING=0 -DENABLE_TESTS=0 
 
-set CH_SLIM_ARGS "-DENABLE_LIBRARIES=0 -DENABLE_LIBURING=0"
-set CH_FAT_ARGS "-DENABLE_LIBRARIES=1"
+set CH_SLIM_ARGS -DENABLE_LIBRARIES=0 -DENABLE_LIBURING=0
+set CH_FAT_ARGS -DENABLE_LIBRARIES=1
 
-set CH_DEBUG_ARGS "-DCMAKE_BUILD_TYPE=Debug -DDEBUG_O_LEVEL=0"
-set CH_RELEASE_ARGS "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+set CH_DEBUG_ARGS -DCMAKE_BUILD_TYPE=Debug -DDEBUG_O_LEVEL=0
+set CH_RELEASE_ARGS -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
 function src -d "Go to main repo of ClickHouse"
   cd $CH_REPO_PATH
 end
 
+function cdPrv --argument-names 'path' -d "Change context to the private repo"
+  if test -n "$path"
+    set CH_REPO_PATH $path
+    src
+  else
+    echo (set_color red) "Private repo path must be provided." (set_color normal)
+  end
+end
+
+function debugPath
+  echo $CH_REPO_PATH/build/Debug | string trim
+end
+
+function releasePath
+  echo $CH_REPO_PATH/build/Release | string trim
+end
+
 function maked -d "Make ClickHouse (slim) in debug mode"
-  mkdir -p $CH_DEBUG_PATH
+  mkdir -p (debugPath)
   pushd $CH_REPO_PATH
-  fish -c "cmake $CH_SLIM_ARGS $CH_DEBUG_ARGS $CH_COMMON -S . -B $CH_DEBUG_PATH"
+  cmake $CH_CC_COMPILER $CH_CXX_COMPILER $CH_COMMON_ARGS $CH_SLIM_ARGS $CH_DEBUG_ARGS $CH_EXTRA_ARGS -S . -B (debugPath)
   popd
 end
 
 function maker -d "Make ClickHouse (slim) in release mode"
-  mkdir -p $CH_RELEASE_PATH
+  mkdir -p (releasePath)
   pushd $CH_REPO_PATH
-  fish -c "cmake $CH_SLIM_ARGS $CH_RELEASE_ARGS $CH_COMMON -S . -B $CH_RELEASE_PATH"
+  cmake $CH_CC_COMPILER $CH_CXX_COMPILER $CH_COMMON_ARGS $CH_SLIM_ARGS $CH_RELEASE_ARGS $CH_EXTRA_ARGS -S . -B (releasePath)
   popd
 end
 
 function makedf -d "Make ClickHouse (fat) in debug mode"
-  mkdir -p $CH_DEBUG_PATH
+  mkdir -p (debugPath)
   pushd $CH_REPO_PATH
-  fish -c "cmake $CH_FAT_ARGS $CH_DEBUG_ARGS $CH_COMMON -S . -B $CH_DEBUG_PATH"
+  cmake $CH_CC_COMPILER $CH_CXX_COMPILER $CH_COMMON_ARGS $CH_FAT_ARGS $CH_DEBUG_ARGS $CH_EXTRA_ARGS -S . -B (debugPath)
   popd
 end
 
 function makerf -d "Make ClickHouse (fat) in release mode"
-  mkdir -p $CH_RELEASE_PATH
+  mkdir -p (releasePath)
   pushd $CH_REPO_PATH
-  fish -c "cmake $CH_FAT_ARGS $CH_RELEASE_ARGS $CH_COMMON -S . -B $CH_RELEASE_PATH"
+  cmake $CH_CC_COMPILER $CH_CXX_COMPILER $CH_COMMON_ARGS $CH_FAT_ARGS $CH_RELEASE_ARGS $CH_EXTRA_ARGS -S . -B (releasePath)
   popd
 end
 
 function cpl -d "Compile ClickHouse in debug mode"
-  mkdir -p $CH_DEBUG_PATH
-  pushd $CH_DEBUG_PATH
+  mkdir -p (debugPath)
+  pushd (debugPath)
   cmake --build . --parallel --
   popd
 end
 
 function cplr -d "Compile ClickHouse in release mode"
-  mkdir -p $CH_RELEASE_PATH
-  pushd $CH_RELEASE_PATH
+  mkdir -p (releasePath)
+  pushd (releasePath)
   cmake --build . --parallel --
   popd
 end
 
 function runs -d "Start ClickHouse server in debug mode"
   pushd $CH_REPO_PATH
-  $CH_DEBUG_PATH/programs/clickhouse-server
+  eval (debugPath)/programs/clickhouse-server
   popd
 end
 
 function runrs -d "Start ClickHouse server in release mode"
   pushd $CH_REPO_PATH
-  $CH_RELEASE_PATH/programs/clickhouse-server
+  eval (releasePath)/programs/clickhouse-server
   popd
 end
 
 function runc -d "Start ClickHouse client in debug mode"
   pushd $CH_REPO_PATH
-  $CH_DEBUG_PATH/programs/clickhouse-client
+  eval (debugPath)/programs/clickhouse-client
   popd
 end
 
 function runrc -d "Start ClickHouse client in release mode"
   pushd $CH_REPO_PATH
-  $CH_RELEASE_PATH/programs/clickhouse-client
+  eval (releasePath)/programs/clickhouse-client
   popd
-end
-
-function xs -d "Compile and run ClickHouse server in debug mode"
-  cpl
-  runs
-end
-
-function xc -d "Compile and run ClickHouse client in debug mode"
-  cpl
-  runc
 end
 
 function testd --argument-names 'name' -d "Invoke C++ unit test (debug) with the given name"
   if test -n "$name" 
     pushd $CH_REPO_PATH
-    PATH=$CH_DEBUG_PATH/programs:$PATH ./tests/clickhouse-test $name
+    PATH=(debugPath)/programs:$PATH ./tests/clickhouse-test $name
     popd
   else 
     echo (set_color red) "Error: unit test name must be provided." (set_color normal)
@@ -105,7 +120,7 @@ end
 function testr --argument-names 'name' -d "Invoke C++ unit test (release) with the given name"
   if test -n "$name" 
     pushd $CH_REPO_PATH
-    PATH=$CH_RELEASE_PATH/programs:$PATH ./tests/clickhouse-test $name
+    PATH=(releasePath)/programs:$PATH ./tests/clickhouse-test $name
     popd
   else 
     echo (set_color red) "Error: unit test name must be provided." (set_color normal)
@@ -121,6 +136,16 @@ function makecihappy --argument-names 'commit' -d "Make CI happy by marking test
   else
     echo (set_color red) "Commit SHA must be provided." (set_color normal)
   end
+end
+
+function xs -d "Compile and run ClickHouse server in debug mode"
+  cpl
+  runs
+end
+
+function xrs -d "Compile and run ClickHouse server in release ode"
+  cplr
+  runrs
 end
 
 alias chs=runs
